7bda951b6aa98e6513ea682a8279694242ed2bd9
Georvic Tur <alexanderstower@gmail.com>
2017-02-18 09:11:43 -0400

    Se agregaron los códigos de las primeras preguntas. Hay que acotar que la data debe estar normalizada y que los coeficientes pedidos por la profe no permiten la convergencia

---
+++ proyecto_1/pregunta_dos_crimen_parte_b.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from proyecto import normalizarZ, regresion_lineal_multiple
+
+if __name__ == '__main__':
+    
+    # Se abre el archivo de prueba limpiado
+    with open("x08_copia.txt", "r") as f : 
+        datos_x08 = np.loadtxt( fname=f, 
+                                dtype=float ,
+                                comments="#" ,
+                                delimiter=",",
+                                usecols=(1,2,3,4))
+        f.close()
+    
+    datos_x08 = normalizarZ(datos_x08)
+    
+    dominio = datos_x08[:,0:3]
+    rango = datos_x08[:,3]
+    aprendizaje = [0.00005,0.00001,0.00003,0.00007,0.00009,0.0001]
+    inicial = 0.1
+    
+    
+    for i in range(6):
+        print("APRENDIZAJE: ", aprendizaje[i])
+        coeficientes,iteraciones,errorPorIteracion = regresion_lineal_multiple( dom=dominio, 
+                                                                                rango=rango,
+                                                                                coeficiente_aprendizaje=aprendizaje[i],
+                                                                                valor_inicial=inicial)
+        errorPorIteracion = np.array(errorPorIteracion)
+        mejor_iter = np.where(errorPorIteracion == errorPorIteracion.min())      # Mejor Iteracion
+        print("Mejor Iteracion", mejor_iter[0][0])
+        print(coeficientes[mejor_iter[0][0]], iteraciones)
+        plt.subplot(3,3,i+1)
+        plt.plot(range(iteraciones), errorPorIteracion)
+        plt.title("Convergencia -Homicidios- (normalizado)")
+        plt.xlabel("Numero de Iteraciones (a={:f})".format(aprendizaje[i]))
+        plt.ylabel("Error")
+        #plt.text(5,0.73, 'Aprendizaje = {:f}'.format(aprendizaje))
+        #plt.text(5,0.7208585, 'Inicial = {:f}'.format(inicial))
+        
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
+    
+    plt.show()
---
+++ proyecto_1/pregunta_dos_parte_a.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from proyecto import normalizarZ, regresion_lineal_multiple
+
+if __name__ == '__main__':
+    
+    # Se abre el archivo de prueba limpiado
+    with open("x01_copia.txt", "r") as f : 
+        datos_x01 = np.loadtxt( fname=f, 
+                                dtype=float ,
+                                comments="#" ,
+                                delimiter=",",
+                                usecols=(1,2))
+        f.close()
+    
+    datos_x01 = normalizarZ(datos_x01)
+    
+    dominio = datos_x01[:,0]
+    rango = datos_x01[:,1]
+    
+    aprendizaje = 0.000001
+    inicial = 1.0
+    coeficientes,iteraciones,errorPorIteracion = regresion_lineal_multiple( dom=dominio, 
+                                                                            rango=rango,
+                                                                            coeficiente_aprendizaje=aprendizaje,
+                                                                            valor_inicial=inicial)
+    
+    errorPorIteracion = np.array(errorPorIteracion)
+    mejor_iter = np.where(errorPorIteracion == errorPorIteracion.min())     # Mejor Iteracion
+    print("Mejor Iteracion: ", mejor_iter[0][0])
+    print(coeficientes[mejor_iter[0][0]], iteraciones)
+    
+    plt.plot(range(iteraciones), errorPorIteracion, 'x')
+    plt.title("Curva de Convergencia -Peso- (normalizado)")
+    plt.xlabel("Numero de Iteraciones")
+    plt.ylabel("Error")
+    plt.text(10,1.131671, 'Aprendizaje = {:f}'.format(aprendizaje))
+    plt.text(10,1.12, 'Inicial = {:f}'.format(inicial))
+    plt.show()

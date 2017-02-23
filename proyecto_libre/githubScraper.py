7932843f4bdc31cdacf7d02998fa610da0813c34
al3x4nd3r <alexanderstower@gmail.com>
2017-02-23 17:16:08 +0000

    añadido spider para recolectar usuarios por a partir de usuario raíz y sus followers hasta una profundidad dada

--- proyecto_libre/githubScraper.py
+++ proyecto_libre/githubScraper.py
@@ -6,11 +6,12 @@ import json
 from auth import username, password
 from requests.auth import HTTPBasicAuth
 import requests
+from collections import deque
 
 """
 #   USAR python3
 #
-#   Para correr la función de scrapping es necesario que registre un token
+#   Para correr la función de scraping es necesario que registre un token
 #   de acceso en un archivo llamado "auth". Dicho archivo sólo va a contener
 #   las variables username y password. Estos son su nombre de usuario de Github
 #   y su token de acceso generado en la configuración. Usar esto le permitirá
@@ -49,19 +50,71 @@ def findUserLanguages(user):
     return languages
 
 
-with open("usuariosGithub.txt", "r") as usersFile :
-    # Si se encontrase un rate limit no se debe permitir que se pierda
-    # el trabajo hecho
-    try :
-        for user in usersFile :
-            print(user.strip("\n\r"))
-            languages.update(findUserLanguages(user.strip("\n\r")))
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    finally :
-        usersFile.close()
 
-with open("languagesUsersGithub.json","w") as langsFile :
-    json.dump(languages, langsFile, indent=4)
-    langsFile.close()
-print(languages)
+def githubUserSpider(user, depth) :
+    users = deque()
+    users.append(user)
+    newLevel = deque()
+    done = deque()
+    lengthUsers = 1
+    lengthNewLevel = 0
+    currentLevel = 0
+    while (currentLevel < depth) :
+        if lengthUsers == 0 :
+            if lengthNewLevel == 0 :
+                return done
+            users = newLevel
+            newLevel = deque()
+            currentLevel += 1
+            lengthUsers = lengthNewLevel
+            lengthNewLevel = 0
+        currentUser = users.popleft()
+        print(currentUser)
+        lengthUsers -= 1
+        done.append(currentUser)
+        response = requests.get('https://api.github.com/users/{0}/followers'.format(currentUser), 
+                                auth=HTTPBasicAuth(username, password)).text
+        responseJson = json.loads(response)
+        for follower in responseJson :
+            if (users.count(follower["login"]) == 0) \
+                and (done.count(follower["login"]) == 0) \
+                and (newLevel.count(follower["login"]) == 0):
+                newLevel.append(follower["login"])
+                lengthNewLevel += 1
+        
+    
+    return done
+
+def findUsers(rootUserName, depth, userListFileName) :
+    usersCrawled = githubUserSpider(rootUserName, depth)
+    with open(userListFileName, "w") as userList:
+        userList.writelines(map(lambda x : x+"\n",usersCrawled))
+        userList.close()
+
+
+if __name__ == '__main__':
+    
+    # Encuentro los usuarios
+    
+    print("\nRecolectando Usernames: ")
+    findUsers("J0hnG4lt", 4, "usuariosGithub.txt")
+    
+    # Encuentro los lenguajes
+    
+    print("\nRecolectando Lenguajes: ")
+    with open("usuariosGithub.txt", "r") as usersFile :
+        # Si se encontrase un rate limit no se debe permitir que se pierda
+        # el trabajo hecho
+        try :
+            for user in usersFile :
+                print("Languages: ",user.strip("\n\r"))
+                languages.update(findUserLanguages(user.strip("\n\r")))
+        except requests.exceptions.HTTPError as err:
+            print(err)
+        finally :
+            usersFile.close()
+    
+    with open("languagesUsersGithub.json","w") as langsFile :
+        json.dump(languages, langsFile, indent=4)
+        langsFile.close()
+    

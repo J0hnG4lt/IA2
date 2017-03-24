47e5d6d365b074c985b01a6ce0f3ddac2a21b211
al3x4nd3r <alexanderstower@gmail.com>
2017-03-24 01:28:08 +0000

    arreglado problema con el key del dict

--- proyecto_libre/generateCluster.py
+++ proyecto_libre/generateCluster.py
@@ -11,8 +11,8 @@ featureMatrix = getFeatureVectors(data)
 
 print("Saving featureMatrix to featureVectors.csv for Weka")
 ofile = open("featureVectors.csv","w")
-ofile.write(",".join(["lang_column"]+langs))
-for lang in langs:
+ofile.write(",".join(["lang_column"]+list(featureMatrix.keys())))
+for lang in featureMatrix.keys():
     ofile.write("\n")
     ofile.write(",".join([lang]+list(map(str,featureMatrix[lang].values()))))
 ofile.close()

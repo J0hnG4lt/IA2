d88961a4e63addd852b651849927d86757892a6c
Leslie Rodrigues <leslierodrigs@gmail.com>
2017-03-25 17:44:26 -0400

    Se añaden cosas necesarias para generar la nube de palabras

--- proyecto_libre/showClusters.py
+++ proyecto_libre/showClusters.py
@@ -1,19 +1,21 @@
+def showClusters():
+    clusterFile = open("clusters.txt","r")
+    clusters = clusterFile.readlines()
 
 
-clusterFile = open("clusters.txt","r")
-clusters = clusterFile.readlines()
+    clusterN = 0
+    for line in clusters[1:] :
+        lang_and_cluster = line.strip("\n\r").split(",")
+        clusterN = max(clusterN,int(lang_and_cluster[2]))
 
+    lang_by_cluster = {i:[] for i in range(clusterN+1)}
 
-clusterN = 0
-for line in clusters[1:] :
-    lang_and_cluster = line.strip("\n\r").split(",")
-    clusterN = max(clusterN,int(lang_and_cluster[2]))
+    for line in clusters[1:] :
+        lang_and_cluster = line.strip("\n\r").split(",")
+        lang_by_cluster[int(lang_and_cluster[2])].append(lang_and_cluster[1])
 
-lang_by_cluster = {i:[] for i in range(clusterN+1)}
-
-for line in clusters[1:] :
-    lang_and_cluster = line.strip("\n\r").split(",")
-    lang_by_cluster[int(lang_and_cluster[2])].append(lang_and_cluster[1])
-
-for lang in lang_by_cluster:
-	print(lang, ", ".join(lang_by_cluster[lang]))
+    
+    for lang in lang_by_cluster:
+        print(lang, ", ".join(lang_by_cluster[lang])
+   
+    return lang_by_cluster

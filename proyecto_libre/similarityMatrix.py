3580e54ce06c5b698c701f18ee01b4a8764d953c
Erick <ericksilvarodriguez@gmail.com>
2017-03-25 18:15:20 -0400

    opcion de prune colocada

--- proyecto_libre/similarityMatrix.py
+++ proyecto_libre/similarityMatrix.py
@@ -7,12 +7,13 @@ def readLanguages(jsonFilename) :
         dataFile.close()
     return data
 
-def calculateCondProbMatrix(data) :
+def calculateCondProbMatrix(data,pruneLanguages = False) :
     
     langAllUses = dict()
     langUsers = dict()
     amountOfUsers = len(data)
     for userAccount in data:
+        langUsers[userAccount] = {}
         for repo in data[userAccount] :
             # we check each language that was used.
             for lang in data[userAccount][repo]:
@@ -27,28 +28,36 @@ def calculateCondProbMatrix(data) :
     totalBytes = sum(langAllUses.values())
     probAllLangs = {lang:(langAllUses[lang]/amountOfUsers) for lang in langAllUses}
     
-    usedTogether = {lang:lang2:0 \
-                        for lang2 in langAllUses} \
-                            for lang in langAllUses}
+    usedTogether = {lang:{lang2:0 for \
+                            lang2 in langAllUses} \
+                                for lang in langAllUses}
+
+    print ("Read data from: ",amountOfUsers)
+
+    if pruneLanguages:
+        languages = sorted(langAllUses, key=langAllUses.get,reverse=True)[:10]
+        print(languages)
+    else:
+        languages = [lang for lang in langAllUses]
 
 
     # Abnormally long if and for chain 
     #  to check amount of times two languages
     #  are used together by an user. 
     for user in langUsers:
-        for lang in langAllUses:
+        for lang in languages:
             if lang in langUsers[user]:
-                for lang2 in langAllUses:
+                for lang2 in languages:
                     if lang2 in langUsers[user]:
                         usedTogether[lang][lang2] += 1 
 
     condProbMatrix = {lang:{lang2:0.0 \
-                        for lang2 in langAllUses} \
-                            for lang in langAllUses}
+                        for lang2 in languages} \
+                            for lang in languages}
     
-    for lang in langAllUses:
+    for lang in languages:
         uses = langAllUses[lang] # The uses of this language
-        for lang2 in langAllUses:
+        for lang2 in languages:
             # conditional probability of lang given lang2
             condProbMatrix[lang][lang2] = 0 if uses == 0 \
                                             else (usedTogether[lang][lang2]/uses)

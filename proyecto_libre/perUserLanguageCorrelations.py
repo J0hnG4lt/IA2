058c5ea9237a6f3ef857e8123f7858533cd014f6
Erick <ericksilvarodriguez@gmail.com>
2017-03-25 18:43:04 -0400

    correlaciones entre usuarios

---
+++ proyecto_libre/perUserLanguageCorrelations.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from extractFeatures import readLanguages,getFeatureVectors
+import json
+from similarityMatrix import calculateCondProbMatrix,calculateCorrelations
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+import matplotlib.pyplot as plt
+
+
+def plot_correlation(dataframe, filename, title='', corr_type=''):
+    lang_names = dataframe.columns.tolist()
+    tick_indices = np.arange(0.5, len(lang_names) + 0.5)
+    plt.figure()
+    plt.pcolor(dataframe.values, cmap='RdBu', vmin=-1, vmax=1)
+    colorbar = plt.colorbar()
+    colorbar.set_label(corr_type)
+    plt.title(title)
+    plt.xticks(tick_indices, lang_names, rotation='vertical')
+    plt.yticks(tick_indices, lang_names)
+    plt.savefig(filename)
+
+
+if __name__ == '__main__':
+
+    data = readLanguages("languagesUsersGithub.json")
+
+    correlations = calculateCondProbMatrix(data,pruneLanguages=30)
+
+    dataset = pd.DataFrame.from_dict(correlations,orient="index")
+
+    pearson_corr = dataset.corr()
+    plot_correlation(
+        pearson_corr,
+        'pearson_language_correlation.svg',
+        title='Popular GitHub Language Correlations',
+        corr_type='Pearson\'s Correlation')
+
+    spearman_corr = dataset.corr(method='spearman')
+    plot_correlation(
+        spearman_corr,
+        'spearman_language_correlation.svg',
+        title='Popular GitHub Language Correlations',
+        corr_type='Spearman\'s Rank Correlation')
--- proyecto_libre/similarityMatrix.py
+++ proyecto_libre/similarityMatrix.py
@@ -25,12 +25,8 @@ def calculateCondProbMatrix(data,pruneLanguages = -1) :
 
 
             
-    totalBytes = sum(langAllUses.values())
     probAllLangs = {lang:(langAllUses[lang]/amountOfUsers) for lang in langAllUses}
     
-    usedTogether = {lang:{lang2:0 for \
-                            lang2 in langAllUses} \
-                                for lang in langAllUses}
 
     print ("Read data from: ",amountOfUsers)
 
@@ -40,6 +36,9 @@ def calculateCondProbMatrix(data,pruneLanguages = -1) :
     else:
         languages = [lang for lang in langAllUses]
 
+    usedTogether = {lang:{lang2:0 for \
+                            lang2 in languages} \
+                                for lang in languages}
 
     # Abnormally long if and for chain 
     #  to check amount of times two languages
@@ -65,3 +64,50 @@ def calculateCondProbMatrix(data,pruneLanguages = -1) :
 
 
     return condProbMatrix
+
+def calculateCorrelations(data,pruneLanguages=-1):    
+    langAllUses = dict()
+    langUsers = dict()
+    amountOfUsers = len(data)
+    for userAccount in data:
+        langUsers[userAccount] = {}
+        for repo in data[userAccount] :
+            # we check each language that was used.
+            for lang in data[userAccount][repo]:
+                if lang in langAllUses:
+                    langAllUses[lang] += 1
+                else :
+                    langAllUses[lang] = 1
+                langUsers[userAccount][lang] = 1
+
+
+            
+    totalBytes = sum(langAllUses.values())
+    probAllLangs = {lang:(langAllUses[lang]/amountOfUsers) for lang in langAllUses}
+    
+
+    print ("Read data from: ",amountOfUsers)
+
+    if pruneLanguages > 0:
+        languages = sorted(langAllUses, key=langAllUses.get,reverse=True)[:pruneLanguages]
+        print(languages)
+    else:
+        languages = [lang for lang in langAllUses]
+
+
+    usedTogether = {lang:{lang2:0 for \
+                            lang2 in languages} \
+                                for lang in languages}
+
+
+    # Abnormally long if and for chain 
+    #  to check amount of times two languages
+    #  are used together by an user. 
+    for user in langUsers:
+        for lang in languages:
+            if lang in langUsers[user]:
+                for lang2 in languages:
+                    if lang2 in langUsers[user]:
+                        usedTogether[lang][lang2] += 1 
+
+    return usedTogether

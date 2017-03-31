03aa2cd07433a0ff05f69a7ed35ea107b0017d00
Erick <ericksilvarodriguez@gmail.com>
2017-03-31 01:33:32 -0400

    arreglados nombres de datos

--- proyecto_libre/perUserGenerateDendogram.py
+++ proyecto_libre/perUserGenerateDendogram.py
@@ -32,12 +32,21 @@ if __name__ == '__main__':
 	print("Building Feature Matrix")
 	condMatrix = calculateCondProbMatrix(data,pruneLanguages = nroLenguajes)
 
-	#Inverting matrix to have a distance matrix instead of similarity
+	print("Saving conditional probability matrix")
+	dataset1 = pd.DataFrame.from_dict(condMatrix,orient="index")
+
+	dataset1.to_csv("corridas/ProbabilidadCondicional/MatrizProbabilidadCondicional_" + str(nroLenguajes)+".csv",
+						float_format="%.2f")
+
+
+
+	#Inverting the matrix probabilities 
+	#  to have a distance matrix instead of similarity
 	for lang in condMatrix:
 		for lang2 in condMatrix[lang]:
 	 		condMatrix[lang][lang2] = 1 - condMatrix[lang][lang2]
 
-	# Making de distance matrix symmetric using max "distance"
+	# Making the distance matrix symmetric using max "distance"
 	distDicc = {lang:{} for lang in condMatrix}
 	for lang in condMatrix:
 		for lang2 in condMatrix[lang]:
@@ -47,7 +56,7 @@ if __name__ == '__main__':
 
 	dataset = pd.DataFrame.from_dict(distDicc,orient="index")
 
-	dataset.to_csv("corridas/ProbabilidadCondicional/MatrizProbabilidadCondicional.csv",
+	dataset.to_csv("corridas/ProbabilidadCondicional/MatrizDistProbabilidadCondicional.csv",
 						float_format="%.2f")
 
 	distMatrix, mapping = convertToMatrix(distDicc)
